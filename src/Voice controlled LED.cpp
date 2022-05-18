a44d2761b41ea5aa6194d4e9b1185052375a924d
munimdev <munimzafar100@gmail.com>
2022-05-18 23:05:47 +0500

    control led through voice

---
+++ src/Voice controlled LED.cpp
@@ -0,0 +1,108 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define WLAN_SSID "C-133"
+#define WLAN_PASS "changeme2018"
+
+int LED_BUILTIN = 2;
+
+//Adafruit.io Setup
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT  1883              
+#define AIO_USERNAME "munimzafar"
+#define AIO_KEY "aio_FTOb293pNypmSXXdgJJttbPMCPF9"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe Light1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME"/feeds/led"); // FeedName
+
+
+void MQTT_connect();
+
+void setup() {
+  Serial.begin(115200);
+
+  // pinMode(Relay1, OUTPUT);
+  // pinMode(Relay2, OUTPUT);
+  // pinMode(Relay3, OUTPUT);
+  // pinMode(Relay4, OUTPUT);
+   
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); 
+  Serial.println(WiFi.localIP());
+ 
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&Light1);
+}
+
+void loop() {
+ 
+  MQTT_connect();
+  
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(20000))) {
+    if (subscription == &Light1) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light1.lastread);
+      int Light1_State = atoi((char *)Light1.lastread);
+      // digitalWrite(Relay1, Light1_State);
+      digitalWrite(LED_BUILTIN, Light1_State);
+      
+    }
+  }
+}
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 5;
+  
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+  
+}
