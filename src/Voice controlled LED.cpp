265e8e1af0c21658ee9ee9ce4c4a3d7ab214c707
munimdev <munimzafar100@gmail.com>
2022-05-19 09:21:07 +0500

    update api

--- src/Voice controlled LED.cpp
+++ src/Voice controlled LED.cpp
@@ -3,8 +3,8 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-#define WLAN_SSID "C-133"
-#define WLAN_PASS "changeme2018"
+#define WLAN_SSID "dusk"
+#define WLAN_PASS "munimzafar"
 
 int LED_BUILTIN = 2;
 
@@ -14,18 +14,15 @@ int LED_BUILTIN = 2;
 #define AIO_USERNAME "munimzafar"
 #define AIO_KEY "aio_FTOb293pNypmSXXdgJJttbPMCPF9"
 
-/************ Global State (you don't need to change this!) ******************/
-
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
-// or... use WiFiFlientSecure for SSL
+// can also use WiFiFlientSecure for SSL
 //WiFiClientSecure client;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
 /****************************** Feeds ***************************************/
-
 
 // Setup a feed called 'onoff' for subscribing to changes.
 Adafruit_MQTT_Subscribe Light1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME"/feeds/led"); // FeedName
@@ -35,11 +32,6 @@ void MQTT_connect();
 
 void setup() {
   Serial.begin(115200);
-
-  // pinMode(Relay1, OUTPUT);
-  // pinMode(Relay2, OUTPUT);
-  // pinMode(Relay3, OUTPUT);
-  // pinMode(Relay4, OUTPUT);
    
   pinMode(LED_BUILTIN, OUTPUT);
   // Connect to WiFi access point.
@@ -73,9 +65,7 @@ void loop() {
       Serial.print(F("Got: "));
       Serial.println((char *)Light1.lastread);
       int Light1_State = atoi((char *)Light1.lastread);
-      // digitalWrite(Relay1, Light1_State);
       digitalWrite(LED_BUILTIN, Light1_State);
-      
     }
   }
 }
